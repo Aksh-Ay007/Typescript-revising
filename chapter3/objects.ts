9a727a0ad05cd85651e290649e56f399557cd831
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-08-05 18:47:39 +0400

    objects

---
+++ chapter3/objects.ts
@@ -0,0 +1,53 @@
+
+
+const user={
+    name:'akshay',
+    email:'akk@gmail.com',
+    isActive:true
+}
+
+
+// function createUser({name:string,isPaid:boolean}):string{
+
+//     return 'tesing'
+// }
+
+// createUser({name:'akshay',isPaid:true})
+
+
+
+
+///just simplifying the above code
+
+
+function createUser({name:string,isActive:boolean}){}
+
+//createUser({name:'akshay',isActive:true})
+
+///just simplifying the above code
+
+const users={name:'akshay',isActive:true}
+
+createUser(users)
+
+
+
+
+
+
+//syntax for a function which return a object
+
+function create():{name:string,isPaid:boolean}{
+
+    return {name:'akki',isPaid:true}
+}
+
+create()
+
+
+
+
+
+
+
+export{}
